dd1cfdbb71fa10ff9561d64c9e4d914abbe17366
ahmedesmail07 <ahmedismail332211@gmail.com>
2023-03-26 00:56:03 +0200

    Python Client

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,3 +4,9 @@ endpoint = "https://www.facebook.com"  # Url Path
 response = requests.get(endpoint)  # HTTP Get Request
 
 print(response.text)  # Print The HTML content of the endpoint that refer to facebook
+
+# API => JSON
+# HTTP REQUEST => HTML
+
+print(response.json())  # Print The JSON content of the endpoint that refer to facebook
+print(response.status_code)  # Returns the actual status code in this case it will ==200
